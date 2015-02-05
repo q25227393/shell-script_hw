e3898d9c995829ea7b055290d4d630755db485c2
s1021449 <binchi4566@gmail.com>
2015-02-05 18:22:06 +0800

    =Adding two commends.

--- ss.sh
+++ ss.sh
@@ -8,10 +8,10 @@ do
  echo -e "what do you want to know?\n1.The logged of each user.\n2.The logged of each IP address or server address."
  read ans
  if [ "$ans" == 1 ]; then 
-  last | awk '{print $1}' | sort | uniq -c | sort -n
+  last | awk '{print $1}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}i'
   correct=true
  elif [ "$ans" == 2 ]; then
-  last | awk '{print $3}' | sort | uniq -c | sort -n
+  last | awk '{print $3}' | sort | uniq -c | sort -n -r | awk -F " " 'NR==1 {print $2}i'
   correct=true
  else
   echo "Oops!\nPlease enter again."
